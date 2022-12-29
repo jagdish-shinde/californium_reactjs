903399cb61ef7a48530738fac20bf067f46e5067
Sameer Bajaj <sameer.bajaj@functionup.org>
2022-12-29 11:50:31 +0530

    scrollable list of images

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-import Board from './components/board/board';
 
 function App() {
   return (
-    <div className="App">
-      <Board/>
+    <div class="horizontal-scroll">
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://loremflickr.com/320/240/dog' className='img'  alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;' alt='dummy' className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://loremflickr.com/g/320/240/paris' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;'  alt='dummy'className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://loremflickr.com/320/240/brazil,rio' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;'  alt='dummy'className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://loremflickr.com/320/240/paris,girl/all' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://loremflickr.com/g/320/240/paris,girl/all' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://source.unsplash.com/random' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;'  alt='dummy'className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://source.unsplash.com/random' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;'  alt='dummy'className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://source.unsplash.com/random' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;'  alt='dummy'className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://source.unsplash.com/random' className='img' alt='dummy'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://api.lorem.space/image/face?w=150&amp;amp;amp;amp;' alt='dummy' className='img'></img></div>
+      <div class="horizontal-scroll__item"><img src='https://source.unsplash.com/random' className='img' alt='dummy'></img></div>
     </div>
   );
 }
