b1eb02d335f8d293171a5d8995a82e6997d8bcf9
jaGdish <jagdish.shinde@functionup.org>
2023-01-19 23:44:22 +0530

    add : signin and signup page

---
+++ src/pages/signin.js
@@ -0,0 +1,8 @@
+import SignInComponent from "../components/signin/signin"
+
+export default function SignInPage () {
+
+    return(
+        <SignInComponent/>
+    )
+}
---
+++ src/pages/signup.js
@@ -0,0 +1,7 @@
+import SignUpComponent from "../components/signup/signup"
+
+export default function SignUpPage ( ) {
+    return(
+        <SignUpComponent/>
+    )
+}
