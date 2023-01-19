e8ac0759f122d70472027f421195ad2fcc9e5049
jaGdish <jagdish.shinde@functionup.org>
2023-01-19 23:41:03 +0530

    add : const data and helper functions

---
+++ src/helper.js
@@ -0,0 +1,16 @@
+exports.isValidString = function (value) {
+    if (typeof value === 'undefined' || value === null) return false
+    if (typeof value === 'string' && value.trim().length === 0) return false
+    return true;
+}
+exports.isValidMobile = function (value) {
+    return /^[6-9]\d{9}$/.test(value)
+}
+
+exports.isValidEmailSyntax = (email) => {
+    return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)
+}
+
+exports.isOnlyLetters = (str) => {
+    return /^[A-Za-z\s]+$/.test(str)
+}
