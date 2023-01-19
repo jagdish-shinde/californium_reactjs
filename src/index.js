31c2c9ee427fe1007a6fdbc1b26221db4464c444
jaGdish <jagdish.shinde@functionup.org>
2023-01-19 23:41:33 +0530

    handled : routing and added common css

--- src/index.js
+++ src/index.js
@@ -3,11 +3,37 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { RecoilRoot } from 'recoil';
+import HomePage from './pages/home';
+import SignInPage from './pages/signin';
+import SignUpPage from './pages/signup';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>,
+  },
+  {
+    path : '/signin',
+    element : <SignInPage/>
+  },
+  {
+    path : '/signup',
+    element : <SignUpPage/>
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <RecoilRoot>
+        <RouterProvider router={router} />
+      </RecoilRoot>
   </React.StrictMode>
 );
 
